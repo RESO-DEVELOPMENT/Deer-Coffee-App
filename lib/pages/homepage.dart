2c74ac0d770e90aae15c171ba5e2a1844f6ccd39
Huy301002 <133476933+Huy301002@users.noreply.github.com>
2023-09-22 16:52:23 +0700

    First commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController countrycode = TextEditingController();
+  TextEditingController phoneNumber = TextEditingController();
+
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    countrycode.text = "+84";
+    super.initState();
+  }
+  bool isPhoneNumberValid() {
+    // Biểu thức chính quy để kiểm tra số điện thoại
+    // Kiểm tra số điện thoại có ít nhất 10 chữ số và chỉ chứa số không chữ
+    RegExp regex = RegExp(r'^\d{10}$');
+    return regex.hasMatch(phoneNumber.text);
+  }
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Hình ảnh làm background
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/image.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Nội dung trên hình ảnh
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Column(
+                            children: [
+                              SizedBox(
+                                height: 50,
+                              ),
+                              Container(
+                                child: Icon(
+                                  Icons.cancel,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                      color: Colors.white,
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          // child: Image.asset("assets/login.png"),
+                        ),
+                        Text.rich(
+                          TextSpan(children: [
+                            TextSpan(
+                                text: "Chào mừng bạn đến với ",
+                                style: TextStyle(color: Colors.grey)),
+                          ]),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Deer Coffee",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue[900],
+                              ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 50.0,
+                            vertical: 30.0,
+                          ),
+                          child: Container(
+                            width: 550,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.grey,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                SizedBox(
+                                  width: 40,
+                                  child: TextField(
+                                    controller: countrycode,
+                                    decoration:
+                                        InputDecoration(border: InputBorder.none),
+                                  ),
+                                ),
+                                Text(
+                                  "|",
+                                  style: TextStyle(
+                                    fontSize: 33,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Nhập số điện thoại",
+                                    ),
+                                  ),
+                                ),
+                               
+                              ],
+                              
+                            ),
+                          
+                          ),
+                          
+                        ),
+                        SizedBox(
+                          width: 385,
+                          height: 50,
+                          child:  ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được bấm
+                            // Ví dụ: Gửi mã xác thực
+                          },
+                          child: Text("Đăng nhập"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.grey[300],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        ),
+                        SizedBox(
+                          height: 145,
+                        ),
+                         SizedBox(
+                          
+                          child: Text("Tiếng Việt"),
+                          ),
+                          
+                      ],
+                    ),
+                  
+                  ),
+                  
+                ),
+               
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
